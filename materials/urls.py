a949c566ab239fcf71cebe4f4dce6ebb554066bb
Anna Kruglova <anya.kruglova.16@gmail.com>
2024-07-27 16:54:46 +0300

    24.1

---
+++ materials/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from materials.apps import MaterialsConfig
+from materials.views import (CourseViewSet, LessonCreateAPIView,
+                             LessonDestroyAPIView, LessonListAPIView,
+                             LessonRetrieveAPIView, LessonUpdateAPIView)
+
+app_name = MaterialsConfig.name
+
+router = DefaultRouter()
+router.register(r"courses", CourseViewSet, basename="courses")
+
+urlpatterns = [
+    path("lessons/", LessonListAPIView.as_view(), name="lessons-list"),
+    path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lessons-retrieve"),
+    path("lessons/create/", LessonCreateAPIView.as_view(), name="lessons-create"),
+    path(
+        "lessons/<int:pk>/delete/",
+        LessonDestroyAPIView.as_view(),
+        name="lessons-delete",
+    ),
+    path(
+        "lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lessons-update"
+    ),
+] + router.urls
